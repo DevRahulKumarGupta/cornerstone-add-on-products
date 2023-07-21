f58bf7bce5cc16000004035773122db29d5d2a01
happy <rahulgupta.bay20@gmail.com>
2023-07-21 17:48:02 +0545

    Added filter

--- assets/js/theme/custom/product.js
+++ assets/js/theme/custom/product.js
@@ -5,45 +5,27 @@ import utils from '@bigcommerce/stencil-utils';
 export default class CustomProduct extends PageManager {
   constructor(context) {
     super(context);
-    this.storeFontApi=context.myProductNameId
+    this.storeFontApi = context.myProductNameId
   }
 
   async onReady() {
+    const checkboxDiv = document.getElementById('customProductIds');
     const detailsData = Number(document.getElementById('productPriceDetails').getAttribute('data-product-price').replace('$', ''));
     const changingPrice = document.getElementById('productPriceDetails');
     const idsList = [];
-    const checkboxDiv = document.getElementById('customProductIds');
     document.getElementById('upsell_products').innerText = 'Add-on product';
 
-    const ids = JSON.parse(document.getElementById('customProductIds').getAttribute("value"));
+    const idsAsNumbers = document.getElementById('customProductIds').getAttribute("value").split(',');
+    const ids = idsAsNumbers.map(id => parseInt(id, 10));
 
-    if (ids) {
+    if (ids && checkboxDiv) {
       async function makeBox(ids, storeFontApi) {
         const products = (await fetchProductsWithjquerry(ids, storeFontApi))
         let checkboxesHTML = '';
 
-        // await Promise.all(
-        //   ids.map((id) =>
-        //     new Promise((resolve) => {
-        //       utils.api.product.getById(
-        //         id,
-        //         { params: { debug: "context" } },
-        //         (_, resp) => {
-        //           products.push({
-        //             title: resp.product.title,
-        //             id: resp.product.id,
-        //             price: resp.product.price.without_tax.formatted
-        //           });
-        //           resolve();
-        //         }
-        //       );
-        //     })
-        //   )
-        // );
-
         products.forEach(function (product) {
           checkboxesHTML += '<input type="checkbox" id="' + product.id + '" name="' + product.title + '" value="' + product.price + '"class="form-checkbox" >' +
-            '<label for="' + product.id + '" value="' + product.price + '" class="form-label form-label--alternate form-label--inlineSmall">' + product.title + ' <strong> $'  + product.price + ' </strong></label><br>';
+            '<label for="' + product.id + '" value="' + product.price + '" class="form-label form-label--alternate form-label--inlineSmall">' + product.title + ' <strong> $' + product.price + ' </strong></label><br>';
         });
 
         checkboxDiv.innerHTML = checkboxesHTML;
@@ -119,95 +101,94 @@ export default class CustomProduct extends PageManager {
         console.error('Error:', error);
       });
     });
-    // console.log(this.storeFontApi)
 
-
-    // console.log(fetchProductsWithjquerry(productIds, this.storeFontApi))
-  
-    async function fetchProductsWithjquerry(productIds, storeFontApi){
+    async function fetchProductsWithjquerry(productIds, storeFontApi) {
       let Authorization = storeFontApi
-    const query = `query SrcsetImages($productIds: [Int!]!) {
-      site {
-        products(entityIds: $productIds) {
-          edges {
-            node {
-              entityId
-              name
-              # Add more fields you want to retrieve for each product here
-              prices {
- price {
-   ...MoneyFields
- }
- priceRange {
-   min {
-     ...MoneyFields
-   }
-   max {
-     ...MoneyFields
-   }
- }
- salePrice {
-   ...MoneyFields
- }
- retailPrice {
-   ...MoneyFields
- }
- saved {
-   ...MoneyFields
- }
- bulkPricing {
-   minimumQuantity
-   maximumQuantity
-   ... on BulkPricingFixedPriceDiscount {
-     price
-   }
-   ... on BulkPricingPercentageDiscount {
-     percentOff
-   }
-   ... on BulkPricingRelativePriceDiscount {
-     priceAdjustment
-   }
- }
-}
+      const query = `
+      query SrcsetImages($productIds: [Int!]!) {
+        site {
+          products(entityIds: $productIds) {
+            edges {
+              node {
+                entityId
+                name
+                # Add more fields you want to retrieve for each product here
+                prices {
+                  price {
+                    ...MoneyFields
+                  }
+                  priceRange {
+                    min {
+                      ...MoneyFields
+                    }
+                    max {
+                      ...MoneyFields
+                    }
+                  }
+                  salePrice {
+                    ...MoneyFields
+                  }
+                  retailPrice {
+                    ...MoneyFields
+                  }
+                  saved {
+                    ...MoneyFields
+                  }
+                  bulkPricing {
+                    minimumQuantity
+                    maximumQuantity
+                    ... on BulkPricingFixedPriceDiscount {
+                      price
+                    }
+                    ... on BulkPricingPercentageDiscount {
+                      percentOff
+                    }
+                    ... on BulkPricingRelativePriceDiscount {
+                      priceAdjustment
+                    }
+                  }
+                }
+              }
             }
           }
         }
       }
-    }
-    fragment MoneyFields on Money {
-value
-currencyCode
-}`;
-  
-   try{
-    const response = await fetch('/graphql', {
-      method: 'POST',
-      credentials: 'same-origin',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${Authorization}` // use auto-generated token
-      },
-      body: JSON.stringify({
-        query: query,
-        variables: {
-          productIds: productIds
+    
+      fragment MoneyFields on Money {
+        value
+        currencyCode
+      }
+    `;
+
+      try {
+        const response = await fetch('/graphql', {
+          method: 'POST',
+          credentials: 'same-origin',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${Authorization}` // use auto-generated token
+          },
+          body: JSON.stringify({
+            query: query,
+            variables: {
+              productIds: productIds
+            }
+          })
+        });
+
+        const data = await response.json();
+        let ProductData = [];
+
+        for (let i in data.data.site.products.edges) {
+          let singleProduct = data.data.site.products.edges[i].node;
+          ProductData.push({ id: singleProduct.entityId, title: singleProduct.name, price: singleProduct.prices.price.value });
         }
-      })
-    });
 
-    const data = await response.json();
-    let ProductData = [];
-
-    for (let i in data.data.site.products.edges) {
-      let singleProduct = data.data.site.products.edges[i].node;
-      ProductData.push({ id: singleProduct.entityId, title: singleProduct.name, price: singleProduct.prices.price.value });
+        return ProductData;
+      } catch (error) {
+        console.error(error);
+        throw error; // Throw the error to be handled elsewhere if needed
+      }
     }
-
-    return ProductData;
-   }catch (error) {
-    console.error(error);
-    throw error; // Throw the error to be handled elsewhere if needed
-  }
-}
   }
 }
